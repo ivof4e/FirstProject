6a971a45ed7c158a3a17757709d539e63e9904fd
ivof4e <ivailo46inov@abv.bg>
2018-10-09 21:35:59 +0300

    first commit upload all project

---
+++ site/java_slideshow.js
@@ -0,0 +1,32 @@
+var index = 1;
+			
+			function plusIndex(n){
+				index = index + n;
+				showImage(index);
+			}
+					
+			showImage(1);
+					
+			function showImage(n){
+				var i;
+				var x = document.getElementsByClassName("slideshow");
+				if(n > x.length){ index = 1};
+				if(n < 1){ index = x.length};
+				for(i=0;i<x.length;i++){
+				x[i].style.display = "none";
+				}
+				x[index-1].style.display = "block";
+			}
+			
+			autoSlide();
+			function autoSlide(){
+				var i;
+				var x = document.getElementsByClassName("slideshow");
+				for(i=0;i<x.length;i++){
+				x[i].style.display = "none";
+				}
+				if(index > x.length){index = 1}
+				x[index-1].style.display = "block";
+				index++;
+				setTimeout(autoSlide,2000);
+			}
